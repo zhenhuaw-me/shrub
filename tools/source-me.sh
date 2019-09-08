4bff4de46342c884437ce462ecca88f6a7668df3
王振华 (Zhenhua WANG) <i@jackwish.net>
2019-09-08 22:09:38 +0800

    tools: let source-me compatibal with macOS

--- tools/source-me.sh
+++ tools/source-me.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-export PYTHONPATH=${PYTHONPATH}:$(dirname $(dirname $(readlink -f $0)))/python/
+
+if [ "$(uname -s)" = "Darwin" ]; then
+  export PYTHONPATH=${PYTHONPATH}:$(dirname $(dirname $(greadlink -f $0)))/python/
+else
+  export PYTHONPATH=${PYTHONPATH}:$(dirname $(dirname $(readlink -f $0)))/python/
+fi
