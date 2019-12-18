a984ff80aa15b23764a79ef6b43abdfa0c337c98
王振华 (Zhenhua WANG) <i@jackwish.net>
2019-12-18 17:02:42 +0800

    tvm: focus on upstream

--- shrub/tvm.py
+++ shrub/tvm.py
@@ -1,13 +1,6 @@
 import tvm, os, logging, nnvm, numpy as np
-from tvm import autotvm
+from tvm import autotvm, relay
 import tvm.contrib.graph_runtime as runtime
-
-# Which codebase are we using
-IsUpstream = True
-try:
-    from tvm import relay
-except ImportError:
-    IsUpstream = False
 
 class Tracker:
   def __init__(self, ip='11.163.182.45', port=20093):
@@ -62,7 +55,7 @@ class TargetProvider:
       self.rpc = RpcInfo(key, '/home/wzh/rpc.tvm/arm32/')
       os.environ['TVM_NDK_CC'] = 'arm-linux-gnueabihf-g++'
     elif key == 'pc1':
-      self.target = 'opencl -device=mali'
+      self.target = 'opencl -device=intel_gpu'
       self.host = 'llvm'
       self.rpc = RpcInfo('pc1', '/home/wzh/tvm/rpc.tvm/')
       os.environ['TVM_NDK_CC'] = 'g++'
@@ -76,17 +69,9 @@ class TargetProvider:
 class Deployables:
   def build(self, target, model, net, params, dumpIR=False, opt_level=3):
     logging.info("Compiling...")
-    if IsUpstream:
-      with relay.build_config(opt_level=opt_level):
-        self.graph, self.lib, self.params = relay.build(
-            net, target=target.target, target_host=target.host, params=params)
-    else:
-      with nnvm.compiler.build_config(opt_level=opt_level):
-        with tvm.build_config(dump_pass_ir=dumpIR):
-          self.graph, self.lib, self.params = nnvm.compiler.build(
-                net, target=target.target, target_host=target.host,
-                shape=model.dict('input', 'shape'),
-                dtype=model.dict('input', 'dtype'), params=params)
+    with relay.build_config(opt_level=opt_level):
+      self.graph, self.lib, self.params = relay.build(
+          net, target=target.target, target_host=target.host, params=params)
 
   def export(self, target, export_path=None):
     if export_path is None:
@@ -108,11 +93,6 @@ class Deployables:
     elif 'opencl' in target.target:
       self.lib.imported_modules[0].save(os.path.join(export_path, 'lib.cl'), 'cl')
 
-    if not IsUpstream:
-      path_json = os.path.join(export_path, 'deploy_graph.json')
-      with open(path_json, 'w') as fo:
-        fo.write(self.graph.json())
-
     path_params = os.path.join(export_path, 'deploy_param.params')
     with open(path_params, 'wb') as fo:
       fo.write(nnvm.compiler.save_param_dict(self.params))
@@ -122,10 +102,8 @@ class RuntimeWrapper:
     if target.rpc.path:
         # upload module to device
         logging.info("Upload...")
-        if IsUpstream:
-            remote = autotvm.measure.request_remote(target.rpc.key, target.tracker.ip, target.tracker.port, timeout=1000)
-        else:
-            remote = autotvm.measure.request_remote(target.rpc.key, tracker_addr=target.tracker.asTuple(), timeout=1000)
+        remote = autotvm.measure.request_remote(target.rpc.key, target.tracker.ip,
+                                                target.tracker.port, timeout=1000)
         remote.upload(deployables.path_so, target=target.rpc.path + 'mylib.so')
         logging.info("Load library...")
         rlib = remote.load_module(target.rpc.path + 'mylib.so')
@@ -172,10 +150,12 @@ class RuntimeWrapper:
     logging.info("Mean inference time (std dev): %.2f ms (%.2f ms)" %
                  (np.mean(prof_res), np.std(prof_res)))
 
-    if not IsUpstream:
-      if target.rpc.path:
-        profile_path = target.rpc.path + 'profile.' + '.log'
-        self.module.run_profile(False, 1, times, custom_filename=profile_path)
-      else:
-        self.module.run_profile(False, 1, times)
+    # if not IsUpstream:
+    #   if target.rpc.path:
+    #     profile_path = target.rpc.path + 'profile.' + '.log'
+    #     self.module.run_profile(False, 1, times, custom_filename=profile_path)
+    #   else:
+    #     self.module.run_profile(False, 1, times)
 
+
+# class Tuner:
