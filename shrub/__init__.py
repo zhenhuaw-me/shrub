5080739dc8e4cfc1b5fead5cdd89341d163f3ec6
王振华 (Zhenhua WANG) <i@jackwish.net>
2020-08-01 22:34:01 +0800

    v0.0.2-post2 for quantization support

--- shrub/__init__.py
+++ shrub/__init__.py
@@ -10,7 +10,7 @@ from shrub import util
 
 # package metadata
 NAME = 'shrub'
-VERSION = '0.0.2-post1'
+VERSION = '0.0.2-post2'
 DESCRIPTION = "Toys to play around with machine learning"
 LICENSE = 'Apache License Version 2.0'
 
