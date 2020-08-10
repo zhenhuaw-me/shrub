add5e9f829181ba25a4e0a6776657e5fa5d7f28f
王振华 (Zhenhua WANG) <i@jackwish.net>
2020-08-10 23:22:02 +0800

    v0.0.2.post5

--- shrub/__init__.py
+++ shrub/__init__.py
@@ -10,7 +10,7 @@ from shrub import util
 
 # package metadata
 NAME = 'shrub'
-VERSION = '0.0.2-post4'
+VERSION = '0.0.2.post5'
 DESCRIPTION = "Toys to play around with machine learning"
 LICENSE = 'Apache License Version 2.0'
 
