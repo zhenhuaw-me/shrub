1230991be9f1fdcdf51104f0d72d9e8eb7e65bcb
王振华 (Zhenhua WANG) <i@jackwish.net>
2020-09-11 23:10:11 +0800

    update docs

--- shrub/__init__.py
+++ shrub/__init__.py
@@ -13,18 +13,4 @@ NAME = 'shrub'
 VERSION = '0.0.2.post5'
 DESCRIPTION = "Toys to play around with machine learning"
 LICENSE = 'Apache License Version 2.0'
-
-__all__ = [
-    network,
-    onnx,
-    tflite,
-    mapping,
-    util,
-    Classifier,
-    TFLiteRunner,
-    ONNXRunner,
-    NAME,
-    VERSION,
-    DESCRIPTION,
-    LICENSE,
-]
+__version__ = VERSION
